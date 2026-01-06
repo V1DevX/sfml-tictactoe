5d422b51594f943d2294976eb5cc5f923806df68
V1DevX <drokigame.12@gmail.com>
2026-01-06 18:54:36 +0600

    Changed spaces to Tabs (size 2)

--- src/App.cpp
+++ src/App.cpp
@@ -3,47 +3,47 @@
 App::App()
 : window(sf::VideoMode(sf::Vector2u{800, 600}), "SFML Template")
 {
-    window.setVerticalSyncEnabled(true);
+	window.setVerticalSyncEnabled(true);
 }
 
 void App::run() {
-    while (window.isOpen()) {
-        handleEvents();
+	while (window.isOpen()) {
+		handleEvents();
 
-        const float dt = frameClock.restart().asSeconds();
-        if (!paused)
-            update(dt);
+		const float dt = frameClock.restart().asSeconds();
+		if (!paused)
+			update(dt);
 
-        render();
-    }
+		render();
+	}
 }
 
 void App::handleEvents() {
-    while (auto event = window.pollEvent()) {
+	while (auto event = window.pollEvent()) {
 
-        // close event
-        if (event->is<sf::Event::Closed>()) {
-            window.close();
-            continue;
-        }
+		// close event
+		if (event->is<sf::Event::Closed>()) {
+			window.close();
+			continue;
+		}
 
-        // key pressed
-        if (const auto* keyEvent = event->getIf<sf::Event::KeyPressed>()) {
-            if (keyEvent->code == sf::Keyboard::Key::Escape) {
-                window.close();
-            }
-            else if (keyEvent->code == sf::Keyboard::Key::Space) {
-                paused = !paused;
-            }
-        }
-    }
+		// key pressed
+		if (const auto* keyEvent = event->getIf<sf::Event::KeyPressed>()) {
+			if (keyEvent->code == sf::Keyboard::Key::Escape) {
+				window.close();
+			}
+			else if (keyEvent->code == sf::Keyboard::Key::Space) {
+				paused = !paused;
+			}
+		}
+	}
 }
 
 void App::update(float /*dt*/) {
-    // future simulation logic
+	// future simulation logic
 }
 
 void App::render() {
-    window.clear(sf::Color(30, 30, 30));
-    window.display();
+	window.clear(sf::Color(30, 30, 30));
+	window.display();
 }
